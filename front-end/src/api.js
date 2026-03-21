1f9ba925403d7234c7cbe29e0d3d7e46d516b4ba
saikiran deshmukh <saikirandeshmukh28@gmail.com>
2026-03-21 04:59:14 -0700

    updated

--- front-end/src/api.js
+++ front-end/src/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const BASE = "https://fin-vault.onrender.com/api";
+
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: BASE,
 });
 
 // Attach token to every request automatically
@@ -14,22 +16,23 @@ API.interceptors.request.use((config) => {
 // Auth
 export const loginUser    = (data) => API.post("/auth/login",    data);
 export const registerUser = (data) => API.post("/auth/register", data);
+export const getMe        = ()     => API.get("/auth/me");
 
 // Transactions
-export const getTransactions   = (params) => API.get("/transactions",      { params });
-export const createTransaction = (data)   => API.post("/transactions",     data);
+export const getTransactions   = (params) => API.get("/transactions",         { params });
+export const createTransaction = (data)   => API.post("/transactions",        data);
 export const deleteTransaction = (id)     => API.delete(`/transactions/${id}`);
 export const getSummary        = (params) => API.get("/transactions/summary", { params });
 
 // Budgets
-export const getBudgets   = (params)   => API.get("/budgets",        { params });
-export const createBudget = (data)     => API.post("/budgets",       data);
-export const updateBudget = (id, data) => API.put(`/budgets/${id}`,  data);
+export const getBudgets   = (params)   => API.get("/budgets",       { params });
+export const createBudget = (data)     => API.post("/budgets",      data);
+export const updateBudget = (id, data) => API.put(`/budgets/${id}`, data);
 
 // Goals
 export const getGoals    = ()          => API.get("/goals");
-export const createGoal  = (data)      => API.post("/goals",              data);
-export const updateGoal  = (id, data)  => API.put(`/goals/${id}`,         data);
+export const createGoal  = (data)      => API.post("/goals",                data);
+export const updateGoal  = (id, data)  => API.put(`/goals/${id}`,           data);
 export const depositGoal = (id, data)  => API.patch(`/goals/${id}/deposit`, data);
 export const deleteGoal  = (id)        => API.delete(`/goals/${id}`);
 
